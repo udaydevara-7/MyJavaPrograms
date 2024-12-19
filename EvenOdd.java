721c3b84889e06eff772826e46b0d7a8cdbf97a6
Udaykiran Devara <161283768+udaydevara-7@users.noreply.github.com>
2024-12-19 20:05:03 +0530

    removed one to ten range numbers

--- EvenOdd.java
+++ EvenOdd.java
@@ -10,10 +10,5 @@ public class EvenOdd{
         else{
             System.out.println(Number+" is odd");
         }
-        for(int i=1;i<=10;i++){
-            if(i%2==0) {
-                System.out.println(i);
-            }
-        }
     }
 }
